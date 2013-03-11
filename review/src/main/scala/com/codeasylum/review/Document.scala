50d210de8ab73df807f114d746523ef75edbe721
David Berkman <david.berkman@grenadefish.net>
2013-03-11 11:17:26 -0700

    removing review project which has gone nowhere, and adding deployment, which will hopefully go somewhere

--- review/src/main/scala/com/codeasylum/review/Document.scala
+++
@@ -1,4 +0,0 @@
-package com.codeasylum.review
-
-class Document {
-}
--- review/src/main/scala/com/codeasylum/review/Space.scala
+++
@@ -1,9 +0,0 @@
-package com.codeasylum.review
-
-class Space (original: String) extends Word(original) {
-
-  override def equals (other: Any): Boolean = other match {
-    case space: Space => true;
-    case _ => super.equals(other)
-  }
-}
