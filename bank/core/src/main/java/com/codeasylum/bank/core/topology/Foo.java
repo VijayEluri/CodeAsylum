c7d7ccb2bad84bdfbe07edca61c72b4a63b3b046
David Berkman <david.berkman@grenadefish.net>
2013-04-19 15:10:39 -0700

    decrement test

--- bank/core/src/main/java/com/codeasylum/bank/core/topology/Foo.java
+++ bank/core/src/main/java/com/codeasylum/bank/core/topology/Foo.java
@@ -1,16 +1,28 @@
 package com.codeasylum.bank.core.topology;
 
+import java.util.LinkedList;
+
 public class Foo {
 
   public static void main (String... args)
     throws Exception {
 
     Topology topology = new Topology("test", new SHA3Partitioner(), 256);
+    LinkedList<Node> nodeList = new LinkedList<>();
 
     for (int loop = 0; loop < 20; loop++) {
 
+      Node node;
+
       System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-      topology.getCircle().join(new Node());
+      topology.getCircle().join(node = new Node());
+      nodeList.add(node);
+      topology.getCircle().foo();
+    }
+
+    for (Node node : nodeList) {
+      System.out.println("################################################################");
+      topology.getCircle().remove(node.getIdentity());
       topology.getCircle().foo();
     }
   }
