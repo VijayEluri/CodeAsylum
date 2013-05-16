eb2ddb4b2be203600ac2cc9d82bf157709874f8d
David Berkman <david.berkman@grenadefish.net>
2013-05-16 11:21:27 -0700

    bank work

--- bank/core/src/main/java/com/codeasylum/bank/core/store/indigenous/JsonConverter.java
+++ bank/core/src/main/java/com/codeasylum/bank/core/store/indigenous/JsonConverter.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2007, 2008, 2009, 2010, 2011 David Berkman
+ * 
+ * This file is part of the CodeAsylum Code Project.
+ * 
+ * The CodeAsylum Code Project is free software, you can redistribute
+ * it and/or modify it under the terms of GNU Affero General Public
+ * License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The CodeAsylum Code Project is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the the GNU Affero General Public
+ * License, along with The CodeAsylum Code Project. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under the GNU Affero GPL version 3 section 7
+ * ------------------------------------------------------------------
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with other code, such other code is not for that reason
+ * alone subject to any of the requirements of the GNU Affero GPL
+ * version 3.
+ */
 package com.codeasylum.bank.core.store.indigenous;
 
 import java.io.IOException;
@@ -87,37 +113,60 @@ public class JsonConverter extends Converter {
         System.out.println(path.toString());
         break;
       case END_OBJECT:
-        path.removeLast();
+        if (!path.isEmpty()) {
+          path.removeLast();
+        }
         System.out.println(path.toString());
         break;
       case END_ARRAY:
         path.removeLast();
+        if ((!path.isEmpty()) && (!path.getLast().equals("array"))) {
+          path.removeLast();
+        }
         System.out.println(path.toString());
         break;
       case VALUE_STRING:
         try {
-
+          return new Column<String>(concatenatePath(), parser.getValueAsString());
         }
         finally {
           path.removeLast();
         }
-
-        break;
       case VALUE_NUMBER_INT:
-        path.removeLast();
-        break;
+        try {
+          return new Column<Long>(concatenatePath(), parser.getValueAsLong());
+        }
+        finally {
+          path.removeLast();
+        }
       case VALUE_NUMBER_FLOAT:
-        path.removeLast();
-        break;
+        try {
+          return new Column<Double>(concatenatePath(), parser.getValueAsDouble());
+        }
+        finally {
+          path.removeLast();
+        }
       case VALUE_TRUE:
-        path.removeLast();
-        break;
+        try {
+          return new Column<Boolean>(concatenatePath(), true);
+        }
+        finally {
+          path.removeLast();
+        }
       case VALUE_FALSE:
-        path.removeLast();
-        break;
+        try {
+          return new Column<Boolean>(concatenatePath(), false);
+        }
+        finally {
+          path.removeLast();
+        }
       case VALUE_NULL:
-        path.removeLast();
-        break;
+        try {
+          return new Column<Void>(concatenatePath(), null);
+        }
+        finally {
+          path.removeLast();
+        }
       default:
         throw new UnknownSwitchCaseException(token.name());
     }
@@ -125,4 +174,20 @@ public class JsonConverter extends Converter {
     return null;
   }
 
+  private String concatenatePath () {
+
+    StringBuilder pathBuilder = new StringBuilder();
+    boolean first = true;
+
+    for (String pathComponent : path) {
+      if (!first) {
+        pathBuilder.append('.');
+      }
+      first = false;
+
+      pathBuilder.append(pathComponent);
+    }
+
+    return pathBuilder.toString();
+  }
 }
--- bank/core/src/main/java/com/codeasylum/bank/core/store/indigenous/RecordShredder.java
+++ bank/core/src/main/java/com/codeasylum/bank/core/store/indigenous/RecordShredder.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2007, 2008, 2009, 2010, 2011 David Berkman
+ * 
+ * This file is part of the CodeAsylum Code Project.
+ * 
+ * The CodeAsylum Code Project is free software, you can redistribute
+ * it and/or modify it under the terms of GNU Affero General Public
+ * License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * The CodeAsylum Code Project is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the the GNU Affero General Public
+ * License, along with The CodeAsylum Code Project. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under the GNU Affero GPL version 3 section 7
+ * ------------------------------------------------------------------
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with other code, such other code is not for that reason
+ * alone subject to any of the requirements of the GNU Affero GPL
+ * version 3.
+ */
 package com.codeasylum.bank.core.store.indigenous;
 
 public class RecordShredder {
@@ -13,7 +39,7 @@ public class RecordShredder {
     throws Exception {
 
     RecordShredder r = new RecordShredder(new JsonConverter("{\"content\": {\n" +
-      "    \"members\": [\n" +
+      "    \"members\": [[\n" +
       "        {\n" +
       "            \"id\": 708,\n" +
       "            \"targetState\": \"ACTIVE\",\n" +
@@ -32,9 +58,11 @@ public class RecordShredder {
       "            \"id\": 105,\n" +
       "            \"targetState\": \"BANNED\"\n" +
       "        }\n" +
-      "    ],\n" +
+      "    ]],\n" +
       "    \"override\": false\n" +
       "}}"));
+
+    r.shred();
   }
 
   public void shred () {
